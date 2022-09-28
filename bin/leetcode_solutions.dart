ae00b833928da0b74058f0871389372992d8aaac
atorulia <33317367+atorulia@users.noreply.github.com>
2022-09-28 13:09:49 +0300

    initial commit

---
+++ bin/leetcode_solutions.dart
@@ -0,0 +1,5 @@
+import 'package:leetcode_solutions/leetcode_solutions.dart' as leetcode_solutions;
+
+void main(List<String> arguments) {
+  print('Hello world: ${leetcode_solutions.calculate()}!');
+}
---
+++ test/leetcode_solutions_test.dart
@@ -0,0 +1,8 @@
+import 'package:leetcode_solutions/leetcode_solutions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
